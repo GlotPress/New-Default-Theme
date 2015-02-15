89daed45b6080e75b6bb489c5459ced2cd377b96
Marko Heijnen <info@markoheijnen.com>
2015-02-15 19:39:25 +0100

    Add no-js support.

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html class="no-js">
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 		<title><?php echo gp_title(); ?></title>
+
+		<script>(function(){document.documentElement.className='js'})();</script>
 
 		<?php gp_head(); ?>
 	</head>
